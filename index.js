dfc770e42156b7cb383ac2b625aaaa73d24db5bb
Radhika Rao <67799531+radhika757@users.noreply.github.com>
2024-11-06 08:41:20 +0530

    connection made successfully

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 // we are specificying where to find mongodb locally and the db.
-mongoose.connect('mongodb://localhost:27017/moviesDB');
+mongoose.connect('mongodb://127.0.0.1:27017/moviesDB');
 
-main().catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect('mongodb://localhost:27017/moviesDB');
-}
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('CONNECTION OPEN');
+})
