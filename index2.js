368cf8e8a849f96723072a0fd2267da26bb16101
Radhika Rao <67799531+radhika757@users.noreply.github.com>
2024-11-11 21:51:05 +0530

    schema and seeds file ready

--- index2.js
+++ index2.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/shopApp2')
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(() => {
         console.log('MONGO CONNECTION OPEN')
     })
